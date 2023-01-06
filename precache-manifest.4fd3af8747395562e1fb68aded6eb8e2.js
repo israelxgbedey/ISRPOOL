43d9b3554b82e67e4f13339c655106c54328f5c1
israelxgbedey <88339733+israelxgbedey@users.noreply.github.com>
2023-01-05 22:08:56 -0600

    Add files via upload

---
+++ precache-manifest.4fd3af8747395562e1fb68aded6eb8e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0c95c8630b991d288df",
+    "url": "/ISRPOOL/static/css/main.efe6e39b.chunk.css"
+  },
+  {
+    "revision": "d0c95c8630b991d288df",
+    "url": "/ISRPOOL/static/js/main.d0c95c86.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/ISRPOOL/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "8d5a0f5a885780d345df",
+    "url": "/ISRPOOL/static/js/2.8d5a0f5a.chunk.js"
+  },
+  {
+    "revision": "108ee5469326961e810f",
+    "url": "/ISRPOOL/static/js/runtime~main.108ee546.js"
+  },
+  {
+    "revision": "92f849fbde4bfbb3bd2d4dbcdc55a014",
+    "url": "/ISRPOOL/index.html"
+  }
+];
